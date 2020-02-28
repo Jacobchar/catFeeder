4f168014b9266b66a9234859f1b1f69140f655de
Jake Charlebois <jcharlebois@profireenergy.com>
2020-02-28 15:07:44 -0700

    Updated to use stepper motor with quarter rotations per serving

--- catFeeder.ino
+++ catFeeder.ino
@@ -2,17 +2,23 @@
 // Include Libraries
 #include "Arduino.h"
 #include "Button.h"
-#include "Wire.h"
 #include "RTClib.h"
-#include "Servo.h"
-#include <TFT.h>
+#include "Wire.h"
 #include <SPI.h>
+#include <TFT.h>
 
 // Pin Definitions
-#define PUSHBUTTON_1_PIN_2 2
-#define PUSHBUTTON_2_PIN_2 3
-#define PUSHBUTTON_3_PIN_2 4
-#define SERVO360MICRO_PIN_SIG 5
+#define PUSHBUTTON_1_PIN 2
+#define PUSHBUTTON_2_PIN 3
+#define PUSHBUTTON_3_PIN 4
+#define PUSHBUTTON_4_PIN 5
+#define STEPPER_MOTOR_STEP_PIN 6
+#define STEPPER_MOTOR_DIR_PIN 7
+#define STEPPER_MOTOR_SLEEP_PIN 8
+
+// Motor steps per rotation
+const int STEPS_PER_REV = 200;
+const int STEPS_PER_QUARTER_REV = STEPS_PER_REV / 4;
 
 // Text variables
 #define TITLE_TEXT_SIZE 2
@@ -24,22 +30,18 @@
 #define ROTATION_TEXT_OFFSET 85
 #define ROTATION_TEXT_HEIGHT 119
 
-// Rotation variables
-#define MAX_ROTATIONS 40
-
 // LCD
 #define cs 10
 #define dc 9
 #define rst 8
 
 // Global variables and defines
-
 // Object initialization
-Button pushButton_1(PUSHBUTTON_1_PIN_2);
-Button pushButton_2(PUSHBUTTON_2_PIN_2);
-Button pushButton_3(PUSHBUTTON_3_PIN_2);
+Button addRotation(PUSHBUTTON_1_PIN);
+Button decrementRotation(PUSHBUTTON_2_PIN);
+Button feedNow(PUSHBUTTON_3_PIN);
+Button sleepScreen(PUSHBUTTON_4_PIN);
 RTC_PCF8523 rtcPCF;
-Servo servo360Micro;
 
 // Create an instance of the library
 TFT TFTscreen = TFT(cs, dc, rst);
@@ -50,20 +52,10 @@ DateTime eveningFeedingTime{2000, 1, 1, 16, 30, 0};
 static bool catsFedMorning = false;
 static bool catsFedEvening = false;
 
-// namespace feedingTime
-// {
-// enum Type :
-// {
-//   MORNING,
-//   EVENING
-// };
-// };
-
 // Rotations Global
-static uint8_t numRotations = 0;
+static uint8_t numRotations_ = 1U;
 
-void setupTFT()
-{
+void setupTFT() {
   // Initialize the library
   TFTscreen.begin();
 
@@ -77,7 +69,7 @@ void setupTFT()
   TFTscreen.setTextSize(TITLE_TEXT_SIZE);
   TFTscreen.text("Next Meal In: ", 0, 0);
   TFTscreen.setTextSize(ROTATION_PER_SERVING_TEXT_SIZE);
-  TFTscreen.text("Rot.Per Serv: ", 0, ROTATION_TEXT_HEIGHT);
+  TFTscreen.text("1/4 Rot.Per Serv: ", 0, ROTATION_TEXT_HEIGHT);
 
   // Print an initial rotation number of 0
   TFTscreen.text("00", ROTATION_TEXT_OFFSET, ROTATION_TEXT_HEIGHT);
@@ -90,9 +82,9 @@ void setupTFT()
   TFTscreen.text(timeString, HOUR_TEXT_OFFSET, TIME_TEXT_HEIGHT);
 }
 
-// Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
-void setup()
-{
+// Setup the essentials for your circuit to work. It runs first every time your
+// circuit is powered with electricity.
+void setup() {
   // Setup Serial which is useful for debugging
   // Use the Serial Monitor to view printed messages
   Serial.begin(9600);
@@ -100,26 +92,25 @@ void setup()
     ; // wait for serial port to connect. Needed for native USB
   Serial.println("Serial Connection Established!");
 
-  pushButton_1.init();
-  pushButton_2.init();
-  pushButton_3.init();
-  if (!rtcPCF.begin())
-  {
+  // Initialize the push buttons
+  addRotation.init();
+  decrementRotation.init();
+  feedNow.init();
+  sleepScreen.init();
+
+  if (!rtcPCF.begin()) {
     Serial.println("Couldn't find RTC");
     while (1)
       ;
   }
-  if (!rtcPCF.initialized())
-  {
+  if (!rtcPCF.initialized()) {
     Serial.println("RTC lost power, lets set the time!");
     // following line sets the RTC to the date & time this sketch was compiled
     rtcPCF.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtcPCF.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  else
-  {
+  } else {
     DateTime now = rtcPCF.now();
     Serial.print("The Current Time is: ");
     Serial.print(now.month(), DEC);
@@ -139,24 +130,32 @@ void setup()
   setupTFT();
 }
 
-void rotateServo(uint8_t numRotations)
-{
-  servo360Micro.attach(SERVO360MICRO_PIN_SIG); // Attach the servo to correct pin to control it.
-  servo360Micro.write(180);                    // Turns servo CW in full speed. change the value in the brackets (180) to change the speed. As these numbers move closer to 90, the servo will move slower in that direction.
-  delay(825 * numRotations);                   // Waits for the time specified; On rotation takes X seconds
-  servo360Micro.write(90);                     // Sending 90 stops the servo
-  delay(300);                                  // Delay so the servo has time to stop before we detach it
-  servo360Micro.detach();                      // Release the servo to conserve power. When detached the servo will NOT hold it's position under stress.
+void rotateStepperMotor(uint8_t numRotations_) {
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+
+  // Enable the A4988 chip
+  digitalWrite(enablePin, LOW);
+
+  // Spin motor one rotation
+  for (int x = 0; x < (STEPS_PER_QUARTER_REV * numRotations_); x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(2000);
+  }
+
+  // Disable the A4988 chip
+  digitalWrite(enablePin, HIGH);
 }
 
-void updateTimeOnScreen(DateTime prevTime, DateTime now)
-{
+void updateTimeOnScreen(DateTime prevTime, DateTime now) {
   // Character array for time
   char timeString[4];
 
-  // Compare the previous time to the current time to determine if the time needs to be updated
-  if (prevTime.hour() != now.hour())
-  {
+  // Compare the previous time to the current time to determine if the time
+  // needs to be updated
+  if (prevTime.hour() != now.hour()) {
     // Update hour
     TFTscreen.setTextSize(TIME_TEXT_SIZE);
     // First clear old hour
@@ -169,8 +168,7 @@ void updateTimeOnScreen(DateTime prevTime, DateTime now)
     TFTscreen.stroke(255, 255, 255);
     TFTscreen.text(timeString, HOUR_TEXT_OFFSET, TIME_TEXT_HEIGHT);
   }
-  if (prevTime.minute() != now.minute())
-  {
+  if (prevTime.minute() != now.minute()) {
     // Update minute
     TFTscreen.setTextSize(TIME_TEXT_SIZE);
     // First clear old minute
@@ -185,37 +183,37 @@ void updateTimeOnScreen(DateTime prevTime, DateTime now)
   }
 }
 
-void updateRotations(uint8_t rotations)
-{
+void updateRotations(uint8_t rotations) {
   char numRotationString[4];
 
   // Erase the current number of rotations
   TFTscreen.setTextSize(ROTATION_PER_SERVING_TEXT_SIZE);
-  snprintf(numRotationString, 4, "%0.2d", numRotations);
+  snprintf(numRotationString, 4, "%0.2d", numRotations_);
   TFTscreen.stroke(0, 0, 0);
   TFTscreen.text(numRotationString, ROTATION_TEXT_OFFSET, ROTATION_TEXT_HEIGHT);
 
-  numRotations = rotations;
+  numRotations_ = rotations;
 
   // Update the number of rotations
-  snprintf(numRotationString, 4, "%0.2d", numRotations);
+  snprintf(numRotationString, 4, "%0.2d", numRotations_);
   TFTscreen.stroke(255, 255, 255);
   TFTscreen.text(numRotationString, ROTATION_TEXT_OFFSET, ROTATION_TEXT_HEIGHT);
 }
 
-bool checkAlarm(DateTime now, DateTime alarmTime)
-{
-  return ((now.hour() == alarmTime.hour()) && (now.minute() == alarmTime.minute()) && (now.second() > alarmTime.second()));
+bool checkAlarm(DateTime now, DateTime alarmTime) {
+  return ((now.hour() == alarmTime.hour()) &&
+          (now.minute() == alarmTime.minute()) &&
+          (now.second() > alarmTime.second()));
 }
 
-bool clearAlarm(DateTime now, DateTime alarmTime)
-{
-  return ((now.hour() == alarmTime.hour()) && (now.minute() > alarmTime.minute()) && (now.second() == alarmTime.second()));
+bool clearAlarm(DateTime now, DateTime alarmTime) {
+  return ((now.hour() == alarmTime.hour()) &&
+          (now.minute() > alarmTime.minute()) &&
+          (now.second() == alarmTime.second()));
 }
 
-// Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
-void loop()
-{
+// After setup, it runs over and over again, in an eternal loop.
+void loop() {
   // Get an initial reading and use this variable to store the previous time
   static DateTime prevTime = rtcPCF.now();
 
@@ -225,174 +223,53 @@ void loop()
   updateTimeOnScreen(prevTime, now);
 
   // Increment the amount of rotations
-  if (pushButton_1.onPress())
-  {
-    updateRotations(numRotations == MAX_ROTATIONS ? MAX_ROTATIONS : (numRotations + 1));
-  }
-
-  // Rotate the servo when button pressed
-  if (pushButton_2.onPress())
-  {
-    // Feed the cats immediately
-    rotateServo(numRotations);
+  if (addRotation.onPress()) {
+    updateRotations(numRotations_ == MAX_ROTATIONS ? MAX_ROTATIONS
+                                                   : (numRotations_ + 1));
   }
 
   // Decrement the number of rotations
-  if (pushButton_3.onPress())
-  {
-    updateRotations(numRotations == 0U ? 0U : (numRotations - 1));
+  if (decrementRotation.onPress()) {
+    updateRotations(numRotations_ == 1U ? 1U : (numRotations_ - 1));
+  }
+
+  // Rotate the servo when button pressed
+  if (feedNow.onPress()) {
+    // Feed the cats immediately
+    rotateStepperMotor(numRotations_);
+  }
+
+  // Wakeup/Sleep the TFT screen
+  if (sleepScreen.onPress()) {
   }
 
   // Alarm check - morning
-  if (!catsFedMorning && checkAlarm(now, morningFeedingTime))
-  {
+  if (!catsFedMorning && checkAlarm(now, morningFeedingTime)) {
     // Feed the cats
-    rotateServo(numRotations);
+    rotateStepperMotor(numRotations_);
     catsFedMorning = true;
   }
 
   // Alarm check - evening
-  if (!catsFedEvening && checkAlarm(now, eveningFeedingTime))
-  {
+  if (!catsFedEvening && checkAlarm(now, eveningFeedingTime)) {
     // Feed the cats
-    rotateServo(numRotations);
+    rotateStepperMotor(numRotations_);
     catsFedEvening = true;
   }
 
-  // Having a delay seems to help the system not get bogged down. Since the time is much less than one second though we should not skip the alarm
+  // Having a delay seems to help the system not get bogged down. Since the time
+  // is much less than one second though we should not skip the alarm
   delay(250);
 
   // Only clear the fed boolean once a minute has elapsed
-  if (catsFedMorning && clearAlarm(now, morningFeedingTime))
-  {
+  if (catsFedMorning && clearAlarm(now, morningFeedingTime)) {
     catsFedMorning = false;
   }
 
-  if (catsFedEvening && clearAlarm(now, eveningFeedingTime))
-  {
+  if (catsFedEvening && clearAlarm(now, eveningFeedingTime)) {
     catsFedEvening = false;
   }
 
   // Update old time
   prevTime = now;
 }
-
-// HERE FOR TESTING
-// if (prevTime.second() != now.second())
-// {
-//   // Update second
-//   TFTscreen.setTextSize(TIME_TEXT_SIZE);
-//   // First clear old second
-//   snprintf(timeString, 4, "%0.2d", prevTime.second());
-//   TFTscreen.stroke(0, 0, 0);
-//   TFTscreen.text(timeString, MINUTE_TEXT_OFFSET, TIME_TEXT_HEIGHT);
-
-//   // Then print new second
-//   snprintf(timeString, 4, "%0.2d", now.second());
-//   TFTscreen.stroke(255, 255, 255);
-//   TFTscreen.text(timeString, MINUTE_TEXT_OFFSET, TIME_TEXT_HEIGHT);
-// }
-
-//  static int servoOnTime = 2000;
-//    // If button 1 is pushed increment the longevity of the servo rotation
-//    if (pushButton_1.onPress()) {
-//      servoOnTime += 1000;
-//      Serial.print("Servo Time is now: ");
-//      Serial.println(servoOnTime, DEC);
-//    }
-//
-//    // If button 2 is pushed decrement the longevity of the servo rotation
-//    if (pushButton_2.onPress()) {
-//      servoOnTime -= 1000;
-//      Serial.print("Servo Time is now: ");
-//      Serial.println(servoOnTime, DEC);
-//    }
-//
-//    // If button 3 is pushed print the delay and rotate the servo CW and then CCW for the delay on time amount
-//    if (pushButton_3.onPress()) {
-//      Serial.print("Servo Time is: ");
-//      Serial.println(servoOnTime, DEC);
-//      Serial.println("Rotating ClockWise");
-//      servo360Micro.write(180);
-//      delay(servoOnTime);
-//      Serial.println("Rotating CounterClockWise");
-//      servo360Micro.write(0);
-//      delay(servoOnTime);
-//      // Stop the servo
-//      servo360Micro.write(90);
-//      delay(10);
-//      Serial.println("Stopping Servo");
-//    }
-//    if(menuOption == '1') {
-//    // Mini Pushbutton Switch #1 - Test Code
-//    //Read pushbutton state.
-//    //if button is pressed function will return HIGH (1). if not function will return LOW (0).
-//    //for debounce funtionality try also pushButton_1.onPress(), .onRelease() and .onChange().
-//    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-//    bool pushButton_1Val = pushButton_1.read();
-//    Serial.print(F("Val: ")); Serial.println(pushButton_1Val);
-//
-//    }
-//    else if(menuOption == '2') {
-//    // Mini Pushbutton Switch #2 - Test Code
-//    //Read pushbutton state.
-//    //if button is pressed function will return HIGH (1). if not function will return LOW (0).
-//    //for debounce funtionality try also pushButton_2.onPress(), .onRelease() and .onChange().
-//    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-//    bool pushButton_2Val = pushButton_2.read();
-//    Serial.print(F("Val: ")); Serial.println(pushButton_2Val);
-//
-//    }
-//    else if(menuOption == '3') {
-//    // Mini Pushbutton Switch #3 - Test Code
-//    //Read pushbutton state.
-//    //if button is pressed function will return HIGH (1). if not function will return LOW (0).
-//    //for debounce funtionality try also pushButton_3.onPress(), .onRelease() and .onChange().
-//    //if debounce is not working properly try changing 'debounceDelay' variable in Button.h
-//    bool pushButton_3Val = pushButton_3.read();
-//    Serial.print(F("Val: ")); Serial.println(pushButton_3Val);
-//
-//    }
-//    else if(menuOption == '4') {
-//    // Adafruit PCF8523 Real Time Clock Assembled Breakout Board - Test Code
-//    //This will display the time and date of the RTC. see RTC.h for more functions such as rtcPCF.hour(), rtcPCF.month() etc.
-//    DateTime now = rtcPCF.now();
-//    Serial.print(now.month(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.day(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.year(), DEC);
-//    Serial.print("  ");
-//    Serial.print(now.hour(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.minute(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.second(), DEC);
-//    Serial.println();
-//    delay(1000);
-//    }
-//    else if(menuOption == '5') {
-//    // Continuous Rotation Micro Servo - FS90R - Test Code
-//    // The servo will rotate CW in full speed, CCW in full speed, and will stop  with an interval of 2000 milliseconds (2 seconds)
-//    servo360Micro.attach(SERVO360MICRO_PIN_SIG);         // 1. attach the servo to correct pin to control it.
-//    servo360Micro.write(180);  // 2. turns servo CW in full speed. change the value in the brackets (180) to change the speed. As these numbers move closer to 90, the servo will move slower in that direction.
-//    delay(2000);                              // 3. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-//    servo360Micro.write(0);    // 4. turns servo CCW in full speed. change the value in the brackets (0) to change the speed. As these numbers move closer to 90, the servo will move slower in that direction.
-//    delay(2000);                              // 5. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-//    servo360Micro.write(90);    // 6. sending 90 stops the servo
-//    delay(2000);                              // 7. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-//    servo360Micro.detach();                    // 8. release the servo to conserve power. When detached the servo will NOT hold it's position under stress.
-//    }
-//
-//    if (millis() - time0 > timeout)
-//    {
-//        menuOption = menu();
-//    }
-
-// snprintf(timeString, 9, "%0.2d:%0.2d:%0.2d", now.hour(), now.minute(), now.second());
-
-// TFTscreen.stroke(255, 255, 255);
-// TFTscreen.text(timeString, 8, 45);
-// delay(250);
-// TFTscreen.stroke(0, 0, 0);
-// TFTscreen.text(timeString, 8, 45);
